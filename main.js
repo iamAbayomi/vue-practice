76a530f740de3b31b175d09627cad4b73ffcb9cb
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-06 20:48:29 +0100

    add product details

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ var app = new Vue( {
         product: 'Socks',
         image : "image/socks.jpg",
         inStock : true,
-        inventory: 5 
+        inventory: 5,
+        details : ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 22234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ]
+
        //, image : "image/blue-socks.jpg"
     }
 }
