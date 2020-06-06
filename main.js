8f882915c16cc0950a94ff0e60281aa05ae6cbcd
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-06 21:28:29 +0100

    Changed the background color of socks

--- main.js
+++ main.js
@@ -2,22 +2,34 @@ var app = new Vue( {
     el: '#app',
     data: {
         product: 'Socks',
-        image : "image/socks.jpg",
+        image : "image/Blue Socks.jpg",
+        altText: "Image of Socks",
         inStock : true,
         inventory: 5,
         details : ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 22234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: "image/blue-socks.jpg"
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: "image/green-socks.jpg"
             }
-        ]
+        ],
+        cart: 0
 
        //, image : "image/blue-socks.jpg"
+    },
+    methods : {
+        addToCart(){
+            this.cart +=1
+        },
+        updateProduct(variantImage){
+            this.image = variantImage
+        }
     }
 }
 
